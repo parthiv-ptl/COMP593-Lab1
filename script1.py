0e3ac2cc722d060989b404df2f6932dfec01503d
parthivpatel <ppatel37@flemingcollege.ca>
2024-01-17 11:51:30 -0500

    Made Changes in both files
    -Removed print() from script1.py
    -added one print() in script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from zen of python
 print("Complex is better than complicated")
-print("beautiful is better than ugly")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from zen of python
 print("Flat is better than nested")
 print("Readability counts")
+print("Beautiful is better than ugly")
